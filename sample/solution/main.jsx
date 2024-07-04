177caa2a89a7cea79a2ea9364f3c67932ee65a9d
Tristan-Walker <122651120+Tristan-Walker@users.noreply.github.com>
2024-07-04 15:23:05 -0400

    base-inputs

---
+++ sample/solution/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Menu from './menu.jsx'
+
+ReactDOM.createRoot(document.getElementById('menu')).render(
+  <React.StrictMode>
+    <Menu/>
+  </React.StrictMode>,
+)
