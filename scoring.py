1bd37c33c82fa8b6a880e784d88652830b798447
iCarlyGod <d33chow@uwaterloo.ca>
2024-07-11 17:14:25 -0400

    Update scoring.py
    - Bug fixes
    - Re-evaluated maximums
    - Now works when given a set of coordinates along route, time of day, and mode of transportation

--- scoring.py
+++ scoring.py
@@ -6,9 +6,14 @@ import os
 import re
 import json
 
-route = sys.argv[1]
-timeOfDay = sys.argv[2]
-transportation = sys.argv[3]
+n = len(sys.argv)
+route = []
+for c in range(1,n-2):
+    l = [float(f) for f in sys.argv[c].split(',')]
+    route.append(l)
+
+timeOfDay = sys.argv[n-2]
+transportation = sys.argv[n-1]
 
 
 # Edit timeOfDay, transportation, and routeAreas, to play around with parameters
@@ -36,7 +41,7 @@ for point in route:
         elif point [1] >= maxLat:
             routeAreas[numLatInc-1][0] = True
         else:
-            latIndex = int((route[1]-minLat)/latInc)+1
+            latIndex = int((point[1]-minLat)/latInc)+1
             routeAreas[latIndex][0] = True
     elif point[0] >= maxLon:
         if point[1] < minLat:
@@ -44,24 +49,26 @@ for point in route:
         elif point [1] >= maxLat:
             routeAreas[numLatInc-1][numLonInc-1] = True
         else:
-            latIndex = int((route[1]-minLat)/latInc)+1
+            latIndex = int((point[1]-minLat)/latInc)+1
             routeAreas[latIndex][numLonInc-1] = True
     else:
         if point[1] < minLat:
-            lonIndex = int((route[0]-minLon)/lonInc)+1
+            lonIndex = int((point[0]-minLon)/lonInc)+1
             routeAreas[latIndex][lonIndex] = True
         elif point [1] >= maxLat:
-            lonIndex = int((route[0]-minLon)/lonInc)+1
+            lonIndex = int((point[0]-minLon)/lonInc)+1
             routeAreas[latIndex][lonIndex] = True
         else:
-            latIndex = int((route[1]-minLat)/latInc)+1
-            lonIndex = int((route[0]-minLon)/lonInc)+1
+            latIndex = int((point[1]-minLat)/latInc)+1
+            lonIndex = int((point[0]-minLon)/lonInc)+1
             routeAreas[latIndex][lonIndex] = True
 
-priority = {'Bikes': [0.7, 0.2, 0.03, 0.1, 0.2, 0.6], 'Feet': [0.56, 0, 0.09, 0.15, 0.3, 0.54], 'Transit': [0.14, 0, 0.09, 0.2, 0.2, 0.36]}
-maxBike = 158130
-maxFeet = 126504
-maxTransit = 31626
+priority = {'Bikes': [0.7, 0.2, 0.03, 0.6, 0.9, 0.6], 'Feet': [0.56, 0, 0.09, 0.8, 1, 0.54], 'Transit': [0.14, 0, 0.09, 0.8, 1, 0.36]}
+maxBike = 10256.4
+maxFeet = 8205.12
+maxTransit = 2051.28
+
+max = {'Bikes': maxBike, 'Feet': maxFeet, 'Transit': maxTransit}
 
 file = os.getcwd() + "\data\datasets\\"+ timeOfDay + ".csv"
 
@@ -78,31 +85,34 @@ with open(file, newline='') as csvfile:
 counts = [0,0,0,0,0,0]
 
 # (Below was used to obtain max danger scores for each mode of transportation)
-# for row in lat:
-#     for value in row:
-#         sum = 0
-#         c = 0
-#         for items in value:
-#             sum+=priority['Bikes'][c]*value[c]
-#         if maxBike < sum:
-#             maxBike = sum
-#         sum = 0
-#         c = 0
-#         for items in value:
-#             sum+=priority['Feet'][c]*value[c]
-#         if maxFeet < sum:
-#             maxFeet = sum
-#         sum = 0
-#         c = 0
-#         for items in value:
-#             sum+=priority['Transit'][c]*value[c]
-#         if maxTransit < sum:
-#             maxTransit = sum
+i = 0
+for row in lat:
+    for value in row:
+        if value[0] <= 2500:
+            sum = 0
+            c = 0
+            for items in value:
+                sum+=priority['Bikes'][c]*value[c]
+            if maxBike < sum:
+                maxBike = sum
+            sum = 0
+            c = 0
+            for items in value:
+                sum+=priority['Feet'][c]*value[c]
+            if maxFeet < sum:
+                maxFeet = sum
+            sum = 0
+            c = 0
+            for items in value:
+                sum+=priority['Transit'][c]*value[c]
+            if maxTransit < sum:
+                maxTransit = sum
 
-# print(int(maxBike))
-# print(int(maxFeet)) 
-# print(int(maxTransit))
+print(maxBike)
+print(maxFeet)
+print(maxTransit)
 
+sq = 0
 r = 0
 for row in routeAreas:
     c = 0
@@ -114,6 +124,7 @@ for row in routeAreas:
             counts[3] += lat[r][c][3]
             counts[4] += lat[r][c][4]
             counts[5] += lat[r][c][5]
+            sq+=1
         c+=1
     r+=1
 # print(counts)
@@ -122,4 +133,6 @@ score = 0
 for c in range(6):
     score+=counts[c]*priority[transportation][c]
 
-print(score)
+score = score/sq
+
+print(min(score*100/max[transportation], 100))
